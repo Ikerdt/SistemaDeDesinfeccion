84654e40c36bfe35b7c1afd6ca0892ca0bff115e
Efren Diaz <Efren Diaz>
2020-07-20 22:20:00 -0500

    Main

--- venv/SistemaDeDesinfeccion.py
+++ venv/SistemaDeDesinfeccion.py
@@ -1,8 +1,10 @@
+#!/usr/bin/env python3.7
+
 from tkinter import *
 from tkinter.ttk import *
 import random,time
 from PIL import ImageTk, Image
-import pyautogui
+import pyautogui,sensores
 
 
 
@@ -15,7 +17,7 @@ class App(Tk):
         container.grid_rowconfigure(0, weight=100)
         container.grid_columnconfigure(0, weight=100)
         self.frames = {}
-        for F in (StartPage, PageOne,PageTwo,PageThree):
+        for F in (StartPage, PageOne,PageTwo,PageThree,PageFour):
             frame = F(container, self)
             self.frames[F] = frame
             frame.grid(row=0, column=0, sticky="nsew")
@@ -24,13 +26,15 @@ class App(Tk):
         frame = self.frames[context]
         frame.event_generate("<<ShowFrame>>")
         frame.tkraise()
+
+
 class StartPage(Frame):
     def __init__(self, parent, controller):
         global image,play,progress
         Frame.__init__(self, parent)
-        canvas = Canvas(self, width=1280, height=720)
+        canvas = Canvas(self, width=480, height=280)
         image = Image.open("Imagenes/Inicio.jpg")
-        image = image.resize((1280, 720), Image.ANTIALIAS)
+        image = image.resize((480, 280), Image.ANTIALIAS)
         image= ImageTk.PhotoImage(image)
         canvas.create_image(0, 0, anchor=NW, image=image)
         canvas.pack()
@@ -39,9 +43,9 @@ class StartPage(Frame):
         self.can=canvas
 
         progress = Progressbar(self,orient=HORIZONTAL,
-                               length=1000, mode='determinate')
-        progress.place(x=150,y=680)
-        progress.after(50,lambda : self.refresh_label())
+                               length=450, mode='determinate')
+        progress.place(x=10,y=230)
+        progress.after(1000,lambda : self.refresh_label())
 
     def refresh_label(self):
         global progress
@@ -49,18 +53,19 @@ class StartPage(Frame):
         progress.update_idletasks()
         self.value+=10
         if (self.value==110):
-            progress.after(50, lambda: self.change.show_frame(PageOne))
+            progress.after(1000, lambda: self.change.show_frame(PageOne))
             return()
-        progress.after(50, lambda: self.refresh_label())
+        progress.after(1000, lambda: self.refresh_label())
+
 
 class PageOne(Frame):
     def __init__(self, parent, controller):
         global wait,config_image, lab_wait
         wait=[]
         Frame.__init__(self, parent)
-        config_canvas = Canvas(self,bg="black", width=1280, height=720)
+        config_canvas = Canvas(self,bg="black", width=480, height=280)
         config_image = Image.open("Imagenes/Inicializando.jpg")
-        config_image = config_image.resize((1280, 720), Image.ANTIALIAS)
+        config_image = config_image.resize((480, 280), Image.ANTIALIAS)
         config_image = ImageTk.PhotoImage(config_image)
         config_canvas.create_image(0, 0, anchor=NW, image=config_image)
         config_canvas.pack()
@@ -68,52 +73,44 @@ class PageOne(Frame):
         self.value3 = 0
         for i in range(12):
             imagen=Image.open(f"Imagenes/GIF_Waiting/waiting_{i}.jpg")
-            imagen=imagen.resize((150, 150), Image.ANTIALIAS)
+            imagen=imagen.resize((60, 60), Image.ANTIALIAS)
             wait.append(ImageTk.PhotoImage(imagen))
         self.change = controller
         lab_wait = Label(self,image=wait[0],padding=-5)
-        lab_wait.place(x=565,y=110)
-        lab_wait.after(5, lambda: self.refresh_label2())
+        lab_wait.place(x=213,y=46)
+        lab_wait.after(100, lambda: self.refresh_label2())
 
     def refresh_label2(self):
         global config_image,wait,lab_wait
         lab_wait.configure(image=wait[self.value2])
         self.value2 += 1
-        if (self.value2==11):
+        if (self.value2==12):
             self.value2=0
-            if (self.value3==10):
+            if (self.value3==20):
                 self.value3 += 1
-                lab_wait.after(5, lambda: self.FAN_ON())
-                lab_wait.after(5, lambda: self.refresh_label2())
-            elif (self.value3==15):
+                lab_wait.after(100, sensores.FAN_ON())
+                lab_wait.after(100, lambda: self.refresh_label2())
+            elif (self.value3==25):
                 self.value3 += 1
-                lab_wait.after(5, lambda: self.HUMIDIFIER_ON())
+                lab_wait.after(100, sensores.HUMIDIFIER_ON())
                 lab_wait.after(850, lambda: self.refresh_label2())
-            elif(self.value3==20):
+            elif(self.value3==30):
                 self.change.show_frame(PageTwo)
                 return()
             else:
                 self.value3+=1
-                lab_wait.after(5, lambda: self.refresh_label2())
+                lab_wait.after(100, lambda: self.refresh_label2())
         else:
-            lab_wait.after(5,lambda: self.refresh_label2())
-    def FAN_ON(self):
-        '''Prender Ventilador'''
-        print("Ventilador ON")
-        return ()
-    def HUMIDIFIER_ON(self):
-        """Prender humidificador"""
-        print("HUMIDIFICADOR ON")
-        return()
+            lab_wait.after(100,lambda: self.refresh_label2())
 
 
 class PageTwo(Frame):
     def __init__(self, parent, controller):
         global run_image
         Frame.__init__(self, parent)
-        config_canvas2 = Canvas(self, width=1280, height=720)
+        config_canvas2 = Canvas(self, width=480, height=280)
         run_image = Image.open("Imagenes/Configuracion.jpg")
-        run_image = run_image.resize((1280, 720), Image.ANTIALIAS)
+        run_image = run_image.resize((480, 280), Image.ANTIALIAS)
         run_image = ImageTk.PhotoImage(run_image)
         config_canvas2.create_image(0, 0, anchor=NW, image=run_image)
         config_canvas2.pack()
@@ -129,29 +126,77 @@ class PageTwo(Frame):
     def getorigin(event,control):
         x, y = pyautogui.position()
         print(x, y)
-        if (x > 975 and x < 1200 and y > 170 and y < 320):
+        if (x > 365 and x < 450 and y > 97 and y < 155):
             control.show_frame(PageThree)
             return ()
+        elif (x > 295 and x < 460 and y > 195 and y < 310):
+            control.show_frame(PageFour)
+            return ()
+        return()
 
 
-        #BOTONES
-        '''
-        start_page = Button(self, text="Start Page",image=home,borderwidth=0,highlightthickness = 0, padx=0,pady=0, command=lambda: controller.show_frame(StartPage))
-        start_page.place(x=654, y=10)
-        page_one = Button(self, text="Page One", image=back,borderwidth=0,highlightthickness = 0, padx=0, pady=0,command=lambda: controller.show_frame(PageOne))
-        page_one.place(x=14, y=10)
-        '''
 class PageThree(Frame):
     def __init__(self, parent, controller):
-        global config_main_image
+        global config_main_image,imhere
         Frame.__init__(self, parent)
-        config_canvas3 = Canvas(self, width=1280, height=720)
-        config_main_image = Image.open("Imagenes/Configuracion2.jpg")
-        config_main_image = config_main_image.resize((1280, 720), Image.ANTIALIAS)
+        config_canvas3 = Canvas(self, width=480, height=280)
+        config_main_image = Image.open("Imagenes/Configuracion_A.jpg")
+        imhere='A'
+        config_main_image = config_main_image.resize((480, 280), Image.ANTIALIAS)
         config_main_image = ImageTk.PhotoImage(config_main_image)
-        config_canvas3.create_image(0, 0, anchor=NW, image=config_main_image)
+        image_on_canvas =config_canvas3.create_image(0, 0, anchor=NW, image=config_main_image)
+        #config_canvas3.create_image(0, 0, anchor=NW, image=config_main_image)
         config_canvas3.pack()
-        config_canvas3.bind("<Button 1>", lambda x:self.getorigin(controller))
+        config_canvas3.bind("<Button 1>", lambda x:self.getorigin(controller,config_canvas3,image_on_canvas))
+
+    def getorigin(event,control,canva,image_on_canvas):
+        global test_image,imhere
+        x, y = pyautogui.position()
+        print(x,y)
+        if (x>0 and x<70 and y>50 and y<95):
+            control.show_frame(PageTwo)
+            return ()
+        elif (x > 170 and x < 230 and y > 128 and y < 170):
+            if (imhere!='A'):
+                test_image = Image.open("Imagenes/Configuracion_A.jpg")
+                test_image = test_image.resize((480, 280), Image.ANTIALIAS)
+                test_image = ImageTk.PhotoImage(test_image)
+                canva.itemconfig(image_on_canvas, image=test_image)
+                canva.image = test_image
+                imhere = 'A'
+                return ()
+        elif (x > 170 and x < 230 and y > 190 and y < 230):
+            if (imhere!='B'):
+                test_image = Image.open("Imagenes/Configuracion_B.jpg")
+                test_image = test_image.resize((480, 280), Image.ANTIALIAS)
+                test_image = ImageTk.PhotoImage(test_image)
+                canva.itemconfig(image_on_canvas, image=test_image)
+                canva.image = test_image
+                imhere = 'B'
+                return ()
+        elif (x > 170 and x < 230 and y > 263 and y < 300):
+            if (imhere!='C'):
+                test_image = Image.open("Imagenes/Configuracion_C.jpg")
+                test_image = test_image.resize((480, 280), Image.ANTIALIAS)
+                test_image = ImageTk.PhotoImage(test_image)
+                canva.itemconfig(image_on_canvas, image=test_image)
+                canva.image = test_image
+                imhere='C'
+                return ()
+        return()
+
+
+class PageFour(Frame):
+    def __init__(self, parent, controller):
+        global operation_image
+        Frame.__init__(self, parent)
+        config_canvas4 = Canvas(self, width=480, height=280)
+        operation_image = Image.open("Imagenes/operacion.jpg")
+        operation_image = operation_image.resize((480, 280), Image.ANTIALIAS)
+        operation_image = ImageTk.PhotoImage(operation_image)
+        config_canvas4.create_image(0, 0, anchor=NW, image=operation_image)
+        config_canvas4.pack()
+        config_canvas4.bind("<Button 1>", lambda x:self.getorigin(controller))
 
     def getorigin(event,control):
         x, y = pyautogui.position()
---
+++ venv/sensores.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3.7
+
+
+#GPIO's
+DHT11_PIN=26
+K1_FAN=20
+K2_HUMIDIFIER=21
+K2_OZONE=16
+
+
+def FAN_ON():
+    '''Prender Ventilador'''
+    print("Ventilador ON")
+    return ()
+def HUMIDIFIER_ON():
+    """Prender humidificador"""
+    print("HUMIDIFICADOR ON")
+    return ()
+def OZONE_ON():
+    """Prender generador de ozono"""
+    print("OZONE ON")
+    return ()
+def FAN_OFF():
+    '''Apagar Ventilador'''
+    print("Ventilador OFF")
+    return ()
+def HUMIDIFIER_OFF():
+    """Apagar humidificador"""
+    print("HUMIDIFICADOR OFF")
+    return ()
+def OZONE_OFF():
+    """Apagar generador de ozono"""
+    print("OZONE OFF")
+    return ()
+def GET_LECTURE():
+    """Obtiene lecturas de los sensores"""
+    print("Getting data...")
+    Ozone_Measure=12 #TEST VALUE
+    Temp_Measure=25 #TEST VALUE
+    Hum_Measure=60 #TEST VALUE
+    return (Ozone_Measure,Temp_Measure,Hum_Measure)
+def GATE_OPEN():
+    ''' Abre la compuerta del filtro de ozono'''
+    print("Opening gate...")
+    return()
+def GATE_CLOSE():
+    '''Cierra la compuerta del filtro de ozono'''
+    print("Closing gate...")
+    return()
